fbcd88b3b53eb399158ccb61504be644e3f94908
SpencerSymington <theSpencerSymington@gmail.com>
2019-01-07 18:56:12 -0800

    init commit

---
+++ Boxes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Boxes
+//
+//  Created by Spencer Symington on 2019-01-07.
+//  Copyright © 2019 Penjat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
